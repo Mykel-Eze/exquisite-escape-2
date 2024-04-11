9ba2f50c05af6459747a7dd74f2ffef87ee2018d
Mykel-Eze <michaeleze1997@gmail.com>
2024-04-11 21:33:36 +0100

    minor corrections on empty components

--- components/SearchComponents/EmptySearch.vue
+++ components/SearchComponents/EmptySearch.vue
@@ -1,18 +1,22 @@
 <template>
     <div class="empty-search-container">
         <img src="@/assets/images/search.svg" alt="search">
-        <div class="text-[#141414] text-[24px]">
-            Sorry, we couldn't find any flights that match your criteria
-        </div>
-        <div class="text-[16px]">
-            Try searching nearby airports or alternative dates
-        </div>
+        <div class="text-[#141414] text-[24px] leading-[1.2] my-1">{{ title }}</div>
+        <div class="text-[16px]">{{ description }}</div>
     </div>
 </template>
 
 <script>
 export default {
-    name: "EmptySearch"
+    name: "EmptySearch",
+    props: {
+        title: {
+            type: String
+        }, 
+        description: {
+            type: String
+        }
+    }
 }
 </script>
 
