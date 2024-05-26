28bcae92d9fa728e1b02e8d9c09f93dc5e009f0a
Marquis Abah <marquis.abah@shuttlers.ng>
2024-05-26 11:19:18 +0100

    setup composable files for api factory

--- apiFactory/payment.ts
+++ apiFactory/payment.ts
@@ -1,13 +1,31 @@
 import { axiosInstance } from "./axios.config";
 
-export const paymentApiFactory = {
-  $_save_card_details(data: any) {
-    return axiosInstance.post("/save-card-token", data);
+export const transferApiFactory = {
+  $_check_availability(data: any) {
+    return axiosInstance.post("/transfer/check-availability", data);
   },
-  $_get_saved_cards() {
-    return axiosInstance.get("/get-added-cards");
+  $_booking_details(data: any) {
+    return axiosInstance.post("/transfer/check-availability", data);
   },
-  $_charge_card(data: any) {
-    return axiosInstance.post("/charge", data);
-  }
+  $_fetch_bookings(data: any) {
+    return axiosInstance.post("/transfer/booking-list", data);
+  },
+  $_cancel_booking(data: any) {
+    return axiosInstance.post("/transfer/check-availability", data);
+  },
+  $_book_transfer(data: any) {
+    return axiosInstance.post("/transfer/book-transfer", data);
+  },
+  $_check_availability_route(data: any) {
+    return axiosInstance.post("/transfer/check-availability/route", data);
+  },
+  $_get_countries(data: any) {
+    return axiosInstance.post("/transfer/get-countries", data);
+  },
+  $_get_routes(data: any) {
+    return axiosInstance.post("/transfer/get-routes", data);
+  },
+  $_get_terminus(data: any) {
+    return axiosInstance.post("/transfer/get-terminals", data);
+  },
 };
