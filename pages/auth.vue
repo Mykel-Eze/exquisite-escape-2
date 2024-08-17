cf7990765f2d9bdbf03f2d9ebff4e1d366c4b291
Mykel-Eze <michaeleze1997@gmail.com>
2024-08-17 04:05:35 +0100

    fix:auth-layout now working

--- pages/auth.vue
+++ pages/auth.vue
@@ -17,11 +17,10 @@
   </div>
 </template>
 
-<script>
-export default {
-    name: "SignInView",
-    layout: "authLayout",
-}
+<script setup>
+definePageMeta({
+  layout: "auth-layout"
+})
 </script>
 
 <style src="~/assets/css/auth.css"></style>
